6584986995609b6317cc7c2e52ba21a82d24fedf
lucaswotta <lucaswotta@gmail.com>
2023-04-30 13:11:59 -0300

    Logic newgame

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -5,5 +5,42 @@ import { Injectable } from '@angular/core';
 })
 export class GameService {
 
-  constructor() { }
+  public board: any = [];
+  boardSize: number = 9;
+  activePlayer: string = "X";
+  turnCount: number = 0;
+  isGameRunning: boolean = false;
+  isGameOver: boolean = false;
+  winner: boolean = false;
+
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.activePlayer = "X";
+    this.turnCount = 0;
+    this.isGameRunning = false;
+    this.isGameOver = false;
+    this.winner = false;
+    this.board = this.createBoard();
+  }
+
+  createBoard() {
+    let board = [];
+    for (let i = 0; i<9; i++) {
+      board.push({id:1, state: null});
+    };
+    return board;
+  }
+
+  get getBoard() {
+    return this.board;
+  }
+
+  set setBoard(board: any) {
+    this.board = [ ... board];
+  }
+
 }
+
